24f516ea34d2ca0cb0aa335d397ac1524aaecfe4
Kyle <kylepdockery@gmail.com>
2018-10-31 11:48:01 -0500

    added color and QoL changes

--- CRUDprojectV2/Program.cs
+++ CRUDprojectV2/Program.cs
@@ -10,23 +10,25 @@ namespace CRUDprojectV2
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             ProductRepository repo = new ProductRepository();
-            Thread.Sleep(20);
             Console.WriteLine("Welcome to Best Buy! I understand you're here to  update are catalogue.\n" +
                 "Let's get started with showing you the current catalogue.");
-            
+            Console.ReadLine();
             List<Product> products = repo.ReadCatalogue();  //read
             foreach (var product in products)
             {
                 Console.WriteLine(product.Name + " " + product.Price);
             }
 
-            Console.ReadLine();
+            Console.ReadLine(); 
+
             Console.WriteLine("\nOkay, so I was informed you needed to add a product, what's it called?");
             Product createdProduct = new Product();
             createdProduct.Name = Console.ReadLine();
             Console.WriteLine("Okay, what about the price?\n");
-            createdProduct.Price = Console.Read();
+            createdProduct.Price = Convert.ToDecimal(Console.ReadLine());
             repo.CreateProduct(createdProduct.Name, createdProduct.Price, 1); //create
 
             Console.WriteLine($"Okay, that completes the addition of {createdProduct.Name}. Let's make sure it went through.");
@@ -38,9 +40,11 @@ namespace CRUDprojectV2
 
             Console.WriteLine($"\nGreat! \n{createdProduct.Name} was properly created...\nNow we need to remove a product. " +
                 $"You can just tell me the name of the product you want to removed.");
-            Console.ReadLine();
             Product deletedProduct = new Product();
+
+            
             deletedProduct.Name = Console.ReadLine();
+            
 
             repo.DeleteProductByName(deletedProduct.Name);  //delete
 
@@ -57,9 +61,9 @@ namespace CRUDprojectV2
                 "So what product do you want to alter?.");
             updatedProduct.Name = Console.ReadLine();
             Console.WriteLine("And what is the new price going to be ? ");
-            updatedProduct.Price = Convert.ToDecimal(Console.Read());
+            updatedProduct.Price = Convert.ToDecimal(Console.ReadLine());
             Console.WriteLine("And what is the new category supposed to be ? ");
-            updatedProduct.CategoryID = Convert.ToInt32(Console.Read());
+            updatedProduct.CategoryID = Convert.ToInt32(Console.ReadLine());
             
             repo.UpdateProductPrice(updatedProduct.Name, updatedProduct.Price, 4);//update
             Console.WriteLine("Great! Thanks for all of your help. Have a nice day!");
